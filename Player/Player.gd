e24d8e5dd1fc0779bb7507f7085d7fb48d9bb385
Clayton Sweeten <cjsweeten101@gmail.com>
2018-09-25 21:45:01 -0600

    sheep toggle

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,13 +5,17 @@ extends KinematicBody2D
 # var a = 2
 # var b = "textvar"
 var speed = 500
+var sheep
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	sheep = false
 	pass
 
 func _physics_process(delta):
+	move()
+	set_form()
+	
+func move():
 	var direction = Vector2()
 	if Input.is_action_pressed("move_down"):
 		direction.y = 1
@@ -23,3 +27,9 @@ func _physics_process(delta):
 		direction.x = -1
 	
 	move_and_slide(direction.normalized() * speed)
+
+func set_form():
+	if Input.is_action_just_pressed("toggle_form"):
+		sheep = !sheep
+		$Human.visible = !$Human.visible
+		$Sheep.visible = !$Sheep.visible
