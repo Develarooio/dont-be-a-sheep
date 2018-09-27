5c008754db5fd296cdee09f00158c71774b58d4b
Clayton Sweeten <cjsweeten101@gmail.com>
2018-09-26 21:58:47 -0600

    small refactor

--- Wolf/Wolf.gd
+++ Wolf/Wolf.gd
@@ -2,14 +2,10 @@ extends KinematicBody2D
 
 var direction = Vector2(0,0)
 var speed = 400
-onready var agro = get_node("AgroArea")
-
-func _ready():
-	agro.connect("sheep_detected" , self , "update_sheep_coord")
 
 func _process(delta):
 	move_and_slide(direction * speed)
 	direction = Vector2(0,0)
 
-func update_sheep_coord(sheep_position):
+func _on_AgroArea_sheep_detected(sheep_position):
 	direction = Vector2(sheep_position - position).normalized()
