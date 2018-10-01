22abf3aa593cd6c4137883dff0f49c11d6ba57ca
Clayton Sweeten <cjsweeten101@gmail.com>
2018-09-30 21:08:04 -0600

    angry boi wulf

--- Wolf/Wolf.gd
+++ Wolf/Wolf.gd
@@ -2,28 +2,38 @@ extends KinematicBody2D
 
 var direction = Vector2(0,0)
 var speed = 400
-onready  var ray = get_node("Ray")
+onready  var ray0 = get_node("Ray0")
+onready  var ray1 = get_node("Ray1")
+onready  var ray2 = get_node("Ray2")
+onready  var ray3 = get_node("Ray3")
+var rays = []
+
+func _ready():
+	rays = [ray0, ray1, ray2, ray3]
 
 func _process(delta):
 	move_and_slide(direction * speed)
 	direction = Vector2(0,0)
 
 func _on_AgroArea_sheep_detected(player_body):
-	var sheep_direction = Vector2(player_body.position - position)
-	var rect_coord = get_corners_from_extents(player_body.shape_owner_get_shape(0,0).extents)
-	if path_blocked(sheep_direction, rect_coord):
+	var sheep_direction = Vector2(player_body.get_position() - position)
+	var rect_coord = get_corners_from_extents(player_body.get_shape().extents)
+	if !path_blocked(sheep_direction, rect_coord):
 		direction = sheep_direction.normalized()
 
 func path_blocked(sheep_center, rect_coords):
-	var blocked = false
-	ray.cast_to = sheep_center
-	#print("=======================================")
-	#for coord in rect_coords:
-	#	print(coord + sheep_center)
-	#	ray.cast_to = sheep_center + coord
-	#	if ray.get_collider() != null and !ray.get_collider().is_in_group("obstacles"):
-	#		blocked = true
-	return false
+	var blocked = []
+	for i in range(0, rect_coords.size()):
+		rays[i].cast_to = sheep_center + rect_coords[i]
+		if rays[i].get_collider() != null and rays[i].get_collider().is_in_group("obstacles"):
+			blocked.append(true)
+		else:
+			blocked.append(false)
+			
+	if blocked.has(false):
+		return false
+	else:
+		return true
 
 func get_corners_from_extents(half_extents):
 	var result = []
