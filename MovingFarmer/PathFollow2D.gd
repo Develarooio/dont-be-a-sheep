3a9c21798c3eefb620e7d37c21d8fe5b5e91a0e5
Max Gallant <mcgallant72@gmail.com>
2018-09-30 22:42:23 -0600

    Add Moving Farmer

---
+++ MovingFarmer/PathFollow2D.gd
@@ -0,0 +1,16 @@
+extends PathFollow2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var SPEED = 10
+var IS_PLAYER_SEEN = false
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	if (!$Farmer.can_see_player):
+		offset = offset + SPEED
